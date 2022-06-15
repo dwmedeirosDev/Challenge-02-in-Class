95671a07cf946ab856692583f67e4e7b9f12277f
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-15 12:06:50 -0300

    Done

---
+++ README.txt
@@ -0,0 +1,6 @@
+// Criar uma função que exibe uma mensagem no console
+
+// Criar uma função que recebe um nome como parâmetro e o exiba no console
+
+// Criar uma função que receba seu nome, profissão, e cidade como parâmetros e exiba uma me
+nsagem no console
---
+++ script.js
@@ -0,0 +1,18 @@
+function mensagem(){
+
+}
+console.log('Questão 1 OK!');
+
+// ##################
+
+function mostrarNome(nome){
+    console.log(nome);
+}
+mostrarNome('Questão 2 OK!');
+
+// ##################
+
+function informacoes(nome,profissao,cidade){
+    console.log(nome,profissao,cidade);
+}
+informacoes('Cebolinha', 'Dono da Rua', 'Bairro do Limoeiro');
